ed47283768c83f35748e86a4c9b79c240594c2bb
sammytam0430 <sammytam2005@hotmail.com>
2018-11-26 02:11:40 -0800

    added control for calender

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -1,9 +1,13 @@
 $(document).ready(() => {
 
-  // Time constant
+  // Time
   const today = new Date();
-  const month = today.getMonth() + 1;
-  const year = today.getFullYear();
+  var currentMonth = today.getMonth();
+  var currentYear = today.getFullYear();
+
+  // name of days
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
   // jQuery Constant
   const $calender = $('#calender');
@@ -12,59 +16,81 @@ $(document).ready(() => {
   $calender.append($thead, $tbody);
 
   const $week = $('<tr>');
-
-  // name of days
-  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
+  $thead.append($week);
   days.forEach(e => {
     $week.append($('<th>').text(e));
   });
 
-  $thead.append($week);
+  const $month = $('#month');
 
-  function daysInMonth(month, year) {
-    return new Date(year, month, 0).getDate();
-  }
+  const $prev = $('#prev');
+  $prev.on('click', () => {
+    if (currentMonth === 0) {
+      currentMonth = 11;
+      currentYear--;
+    } else {
+      currentMonth--;
+    }
 
-  function createCalendar() {
+    createCalendar(currentMonth, currentYear);
+  });
 
-    var daysInAMonth = daysInMonth(month, year);
+  const $today = $('#today');
+  $today.on('click', () => {
+    var currentMonth = today.getMonth();
+    var currentYear = today.getFullYear();
+
+    createCalendar(currentMonth, currentYear);
+  });
+
+  const $next = $('#next');
+  $next.on('click', () => {
+    if (currentMonth === 11) {
+      currentMonth = 0;
+      currentYear++;
+    } else {
+      currentMonth++;
+    }
+
+    createCalendar(currentMonth, currentYear);
+  });
+
+  function createCalendar(month, year) {
+    $month.text(`${months[month]} ${year}`);
+    $tbody.text('');
+    var daysInAMonth = new Date(year, month + 1, 0).getDate();
     var dateArr = [];
     var monthArr = [];
-    for (let i = 1; i < daysInAMonth + 1; i++) {
-      let newDate = new Date(year, month - 1, i);
+    for (let i = 1; i <= daysInAMonth; i++) {
+      let newDate = new Date(year, month, i);
       dateArr.push(newDate);
       if (newDate.getDay() == 6) {
         monthArr.push(dateArr);
         dateArr = [];
-      } else if (i === daysInAMonth - 1) {
+      } else if (i === daysInAMonth) {
         monthArr.push(dateArr);
       }
     }
 
     for (let i = 0; i < monthArr.length; i++) {
       let $tr = $('<tr>');
-      if (i === 0) {
-        for (let j = 0; j < 7 - monthArr[0].length; j++) {
-          $tr.append($('<td>'));
-        }
+      if (i === 0 && monthArr[0].length < 7) {
+        $tr.append($(`<td colspan='${7 - monthArr[0].length}'>`));
       }
 
       for (let j  = 0; j < monthArr[i].length; j++) {
         $tr.append($('<td>').text(monthArr[i][j].getDate()));
       }
 
-      if (i === monthArr.length - 1) {
-        let last = monthArr.length - 1;
-        for (let j = 0; j < 7 - monthArr[last].length; j++) {
-          $tr.append($('<td>'));
-        }
+      let last = monthArr.length - 1;
+      if (i === monthArr.length - 1 && monthArr[last].length < 7) {
+        $tr.append($(`<td colspan='${7 - monthArr[last].length}'>`));
       }
 
       $tbody.append($tr);
     }
   }
 
-  createCalendar();
+  createCalendar(currentMonth, currentYear);
 
 });
