8c26a7a41eeb41ff550f2e0235421984dda9781d
gummypepsi <31948781+gummypepsi@users.noreply.github.com>
2018-11-27 09:26:04 -0800

    Add files via upload
    dashboard javascript

---
+++ dashboard.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var innerPrev = document.getElementById("innerContent");
+    innerPrev.innerHTML = "<h1> July 13 - July 28 </h1>"
+    + "<p> $25/hr </p>"
+    + "<p> $36/hr </p>"
+    + "<p> $19/hr </p>";
+
+  }
+
+function myFunction2() {
+  var innerNext = document.getElementById("innerContent");
+  innerNext.innerHTML = "<h1> July 28 - July 31</h1>"
+  + "<p> Requested by student(s): Bring another Basketball </p>"
+  + "<p> Requested by student(s): Alex and John will be late for 10minutes</p>"
+  + "<p> Requested by student(s): Brian may not make the session, info will be updated</p>"
+
+}
