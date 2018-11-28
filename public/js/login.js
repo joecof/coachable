92706b67309d0a2e455a5452c3e2bbd8c17daaba
Sammy Tam <sammytam2005@hotmail.com>
2018-11-27 20:48:46 -0800

    implemented auth and database for login

---
+++ public/js/login.js
@@ -0,0 +1,40 @@
+$(document).ready(() => {
+  var database = firebase.database();
+
+  $loginEmail = $('#email');
+  $loginPassword = $('#password');
+  $login = $('#login');
+  $loginError = $('#loginError')
+
+  $login.on('click', () => {
+    let email = $loginEmail.val();
+    let password = $loginPassword.val().trim();
+
+    $loginError.text('');
+
+    if (email === "") {
+      $loginError.text("Your email cannot be empty!");
+    } else if (password === "") {
+      $loginError.text("Your passwords cannot be empty!");
+    } else {
+      firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(`${errorCode}: ${errorMessage}`);
+        $loginError.text(errorMessage);
+      });
+
+      firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          firebase.database().ref("users/" + user.uid).update(
+            {
+              "email" : user.email
+            }
+          );
+          $(location).attr('href', './schedule');
+        }
+      });
+    }
+  })
+});
